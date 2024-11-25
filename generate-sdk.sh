ee19b591e795b328e2cb4db232dedef21291b74e
basonjar <liudotjson@gmail.com>
2024-11-25 11:10:32 -0800

    Add swagger definition and openapi generator

---
+++ generate-sdk.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Default to snapshot version
+SNAPSHOT_FLAG="snapshot=true"
+
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        --release=true)
+            SNAPSHOT_FLAG=""
+            shift
+            ;;
+        *)
+            echo "Unknown option: $1"
+            exit 1
+            ;;
+    esac
+done
+
+ADDITIONAL_PROPERTIES_LIST=(
+  "npmName=@basonjar/congress-gov-sdk"
+  "supportsES6=true"
+  "withNodeImports=true"
+  "apiPackage=congress-gov"
+  "licenseName=Apache-2.0"
+  "${SNAPSHOT_FLAG}"
+  "useSingleRequestParameter=true"
+)
+ADDITIONAL_PROPERTIES=$(IFS=,; echo "${ADDITIONAL_PROPERTIES_LIST[*]}")
+
+
+openapi-generator-cli generate \
+  -i swagger.yaml \
+  -g typescript-axios \
+  -o ./src \
+  --additional-properties="$ADDITIONAL_PROPERTIES"
+
+echo "SDK generation complete with properties: $ADDITIONAL_PROPERTIES"
